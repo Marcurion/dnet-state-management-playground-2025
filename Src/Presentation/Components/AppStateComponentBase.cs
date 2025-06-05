0dd277b3fa592a3745a31fbd6a9d69af7682334f
marcurion <marcurion@private>
2025-06-05 08:31:53 +0200

    wip

--- Src/Presentation/Components/AppStateComponentBase.cs
+++ Src/Presentation/Components/AppStateComponentBase.cs
@@ -12,6 +12,8 @@ public class AppStateComponentBase : ComponentBase, IDisposable
     public IAppState AppState { get; set; } = default!;
     
     private bool _shouldRender = true;
+    protected string ListAnimationClass = "";
+    protected string ComponentAnimationClass = "";
     
     protected override bool ShouldRender() => _shouldRender;
     
@@ -19,9 +21,23 @@ public class AppStateComponentBase : ComponentBase, IDisposable
     {
         Console.WriteLine("MANUALRENDER TRIGGERED");
         AppState = newState ?? throw new ArgumentNullException(nameof(newState));
+        
+        // Reset animation classes to retrigger animations
+        ListAnimationClass = "";
+        ComponentAnimationClass = "";
+        
         _shouldRender = true;
         StateHasChanged();
         _shouldRender = false;
+        
+        // Apply animation classes after render with small delay
+        _ = Task.Run(async () =>
+        {
+            await Task.Delay(10); // Small delay ensures DOM reflow
+            ListAnimationClass = "list-update-animation";
+            ComponentAnimationClass = "component-refresh-animation";
+            await InvokeAsync(StateHasChanged);
+        });
     }
     
     protected override void OnInitialized()
